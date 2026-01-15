184cf2f2268d5dc325ef1dc94553c97b2b028d85
PhatTV203 <phattv203@gmail.com>
2026-01-15 12:09:38 +0700

    init project

---
+++ src/components/Map/MapContainer.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { MapContainer as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { useEffect } from 'react';
+
+const MapContainer = () => {
+  useEffect(() => {
+    // Fix for default marker icon in Leaflet with Next.js
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    });
+  }, []);
+
+  return (
+    <LeafletMap center={[21.0285, 105.8542]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[21.0285, 105.8542]}>
+        <Popup>
+          Hanoi, Vietnam <br /> Capital City.
+        </Popup>
+      </Marker>
+    </LeafletMap>
+  );
+};
+
+export default MapContainer;
